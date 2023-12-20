9c2041434f0c058f2903afea9cc2b44a06f076b8
Elyahu <Elyahu41@gmail.com>
2023-12-19 17:54:16 -0700

    Initial Commit

---
+++ KosherSwift/KosherSwift.h
@@ -0,0 +1,18 @@
+//
+//  KosherSwift.h
+//  KosherSwift
+//
+//  Created by User on 12/19/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KosherSwift.
+FOUNDATION_EXPORT double KosherSwiftVersionNumber;
+
+//! Project version string for KosherSwift.
+FOUNDATION_EXPORT const unsigned char KosherSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KosherSwift/PublicHeader.h>
+
+
