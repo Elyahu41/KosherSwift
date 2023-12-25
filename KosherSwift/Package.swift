d565619d34e266a53090e4fc35a9b53dfe07e9ba
Elyahu <Elyahu41@gmail.com>
2023-12-25 16:32:09 -0700

    revert

--- KosherSwift/Package.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Package.swift
-//  KosherSwift
-//
-//  Created by User on 12/19/23.
-//
-
-import Foundation
--- Package.swift
+++ Package.swift
@@ -9,10 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KosherSwift",
-            targets: [
-		.target(
-			name:"KosherSwift",
-			path:"KosherSwift"]),
+            targets: ["KosherSwift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
